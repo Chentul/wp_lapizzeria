2edb95dcef11865dc14643c92e5fce2820b9a0b5
vicentenoriega <vicente.noriega.sc@gmail.com>
2017-12-31 17:23:51 -0600

    SE5 - 54. Imprimiendo el Logotipo

--- wp-content/themes/lapizzeria_theme/header.php
+++ wp-content/themes/lapizzeria_theme/header.php
@@ -5,6 +5,19 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta description="" />
 	<title>La Pizzeria | <?php the_title(); ?></title>
-	<?php wp_head(); // Función de WordPress para incluir dependencias ?>
+	<?php 
+		// Función de WordPress para incluir dependencias
+		wp_head();
+	?>
 </head>
 <body>
+	<header class="encabezado-sitio">
+		<div class="contenedor">
+			<div class="logo">
+				<a href="<?php echo esc_url( home_url('/') ); ?>" title="ir a inicio">
+					<img src="<?php echo get_template_directory_uri()?>/assets/img/logo.svg" alt="logo de la pizzeria" />
+				</a>
+			</div> <!-- .logo -->
+		</div> <!-- .contenedor -->
+		
+	</header>
