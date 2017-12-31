f61b5d8a3b254180b8ad126e962e639e4c85073e
vicentenoriega <vicente.noriega.sc@gmail.com>
2017-12-31 17:43:02 -0600

    SE - 55. Creando un Menú de navegación

--- wp-content/themes/lapizzeria_theme/functions.php
+++ wp-content/themes/lapizzeria_theme/functions.php
@@ -1,18 +1,42 @@
 <?php 
 
-// Incluye nuestro archivo style.css de nuestro tema a WordPress
+// incluye nuestro archivo style.css de nuestro tema a WordPress
 function lapizzeria_styles() {
 	
-	// Función para registrar los estilos
+	// función para registrar los estilos
 	wp_register_style( 'normalize', get_template_directory_uri() . '/assets/css/normalize.css', array(), '7.0' );
-	
-	// Se agrego en el parametro array( 'normalize' ) como dependencia, es decir, primero carga normalize y seguidamente nuestro style.css
+
+	// se agrego en el parametro array( 'normalize' ) como dependencia, es decir, primero carga normalize y seguidamente nuestro style.css
 	wp_register_style( 'style', get_template_directory_uri() . '/style.css', array( 'normalize' ), '1.0' );
 
-	// Función para incrustrar los estilos
+	// función para incrustrar los estilos
 	wp_enqueue_style( 'normalize' );
 	wp_enqueue_style( 'style' );
 
 }
 
 add_action( 'wp_enqueue_scripts', 'lapizzeria_styles' );
+
+// función para implementar los menus en nuestro tema de WordPress
+function lapizzeria_menus() {
+
+	register_nav_menus( array(
+		'header-menu' => __( 'Header Menu', 'lapizzeria' ),
+		'social-menu' => __( 'Social Menu', 'lapizzeria' )
+	));
+}
+
+add_action( 'init', 'lapizzeria_menus' );
+
+
+
+
+
+
+
+
+
+
+
+
+
