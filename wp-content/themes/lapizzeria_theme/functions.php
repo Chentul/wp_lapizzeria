e6af909aba44115d6394974b0963a32153f6a5ca
vicentenoriega <vicente.noriega.sc@gmail.com>
2017-12-31 16:51:45 -0600

    SE5 - 50. Agregando Normalize CSS

--- wp-content/themes/lapizzeria_theme/functions.php
+++ wp-content/themes/lapizzeria_theme/functions.php
@@ -2,8 +2,16 @@
 
 // Incluye nuestro archivo style.css de nuestro tema a WordPress
 function lapizzeria_styles() {
+	
+	// Función para registrar los estilos
+	wp_register_style( 'normalize', get_template_directory_uri() . '/assets/css/normalize.css', array(), '7.0' );
+	
+	// Se agrego en el parametro array( 'normalize' ) como dependencia, es decir, primero carga normalize y seguidamente nuestro style.css
+	wp_register_style( 'style', get_template_directory_uri() . '/style.css', array( 'normalize' ), '1.0' );
 
-	wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css', array(), '1.0' );
+	// Función para incrustrar los estilos
+	wp_enqueue_style( 'normalize' );
+	wp_enqueue_style( 'style' );
 
 }
 
