223782b9c9e4de82d43f1b634299525a75b8b754
vicentenoriega <vicente.noriega.sc@gmail.com>
2017-12-31 16:41:30 -0600

    SE5 - 49. Cargando las hojas de estilo en WordPress

---
+++ wp-content/themes/lapizzeria_theme/functions.php
@@ -0,0 +1,10 @@
+<?php 
+
+// Incluye nuestro archivo style.css de nuestro tema a WordPress
+function lapizzeria_styles() {
+
+	wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css', array(), '1.0' );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'lapizzeria_styles' );
--- wp-content/themes/lapizzeria_theme/index.php
+++ wp-content/themes/lapizzeria_theme/index.php
@@ -1,10 +1,21 @@
-<?php 
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>La Pizzeria | <?php the_title(); ?></title>
+	<?php wp_head(); // Función de WordPress para incluir dependencias ?>
+</head>
+<body>
+	<?php 
 	
-	while( have_posts() ) {
+		while( have_posts() ) {
 
-		the_post();
-		the_title( '<h1>', '</h1>' );
-		the_content();
-	}
-	
-?>
+			the_post();
+			the_title( '<h1>', '</h1>' );
+			the_content();
+		}
+		
+	?>
+</body>
+</html>
