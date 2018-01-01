90094aa762c908b5f7ca5071936f9da120357c5b
vicentenoriega <vicente.noriega.sc@gmail.com>
2017-12-31 18:00:31 -0600

    SE5 - 57. Creando un Menu de Redes Sociales

--- wp-content/themes/lapizzeria_theme/header.php
+++ wp-content/themes/lapizzeria_theme/header.php
@@ -18,12 +18,33 @@
 					<img src="<?php echo get_template_directory_uri()?>/assets/img/logo.svg" alt="logo de la pizzeria" />
 				</a>
 			</div> <!-- .logo -->
+			<div class="informacion-encabezado">
+				<div class="redes-sociales">
+					<?php 
+						// argumentos necesarios para imprimir nuestro menu de redes sociales
+						$args = array(
+							'theme_location' => 'social-menu',
+							'container' => 'nav',
+							'container_class' => 'menu-social',
+							'container_id' => 'menu-social',
+							'link_before' => '<span class="sr-text">',
+							'link_after' => '</span>'
+						);
+						// imprime el menu de redes sociales
+						wp_nav_menu( $args );
+					?>
+				</div>	<!-- .redes-sociales -->
+				<div class="direccion">
+					<p>8179 Bay Avenue Mountain View, CA 94043</p>
+					<p>Teléfono: +1-92-456-7890</p>
+				</div>
+			</div><!-- .informacion-encabezado -->
 		</div> <!-- .contenedor -->
 	</header>
 	<nav class="menu-sitio">
 		<div class="contenedor navegacion">
 			<?php 
-				// argumentos necesarios para imprimir nuestro menu
+				// argumentos necesarios para imprimir nuestro menu de páginas
 				$args = array(
 					'theme_location' => 'header-menu',
 					'container' => 'nav',
