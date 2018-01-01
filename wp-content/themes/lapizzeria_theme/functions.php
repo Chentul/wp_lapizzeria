42bd52de79a005dbc05a18d10bf088f2cd7788f9
vicentenoriega <vicente.noriega.sc@gmail.com>
2018-01-01 02:12:22 -0600

    SE5 - 60. Agregando Font Awesome

--- wp-content/themes/lapizzeria_theme/functions.php
+++ wp-content/themes/lapizzeria_theme/functions.php
@@ -2,15 +2,18 @@
 
 // incluye nuestro archivo style.css de nuestro tema a WordPress
 function lapizzeria_styles() {
-	
+
 	// función para registrar los estilos
 	wp_register_style( 'normalize', get_template_directory_uri() . '/assets/css/normalize.css', array(), '7.0' );
+
+	wp_register_style( 'fontawesome', get_template_directory_uri() . '/assets/css/font-awesome.min.css', array( 'normalize' ), '4.7' );
 
 	// se agrego en el parametro array( 'normalize' ) como dependencia, es decir, primero carga normalize y seguidamente nuestro style.css
 	wp_register_style( 'style', get_template_directory_uri() . '/style.css', array( 'normalize' ), '1.0' );
 
 	// función para incrustrar los estilos
 	wp_enqueue_style( 'normalize' );
+	wp_enqueue_style( 'fontawesome' );
 	wp_enqueue_style( 'style' );
 
 }
