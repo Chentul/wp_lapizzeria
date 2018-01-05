9f5aa4e486b9c5f7b3478d689412cc017d831221
vicente noriega <vicente.noriega@thlink.marketing>
2018-01-04 18:03:18 -0600

    SE6 - Creando un Template exclusivo para la página de nosotros

---
+++ wp-content/themes/lapizzeria_theme/page-nosotros.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+
+<?php while( have_posts() ) { the_post(); ?>
+
+  <div class="hero" style="background: url( <?php echo get_the_post_thumbnail_url(); ?>);">
+    <div class="contenido-hero">
+      <div class="texto-hero">
+        <h1><?php the_title(); ?></h1> <!-- imprime el titutlo de la página de WP -->
+      </div>
+    </div> <!-- .contenido-hero -->
+  </div> <!-- .hero -->
+
+  <div class="principal contenedor">
+    <main class="texto-centrado contenido-paginas">
+      <?php the_content(); ?> <!-- imprime el contenido de WP -->
+    </main> <!-- .contenido-paginas -->
+  </div> <!-- .principal contenedor -->
+
+<?php } // fin del while ?>
+
+<script>console.log('page-nosotros.php');</script>
+<?php get_footer(); ?>
--- wp-content/themes/lapizzeria_theme/page.php
+++ wp-content/themes/lapizzeria_theme/page.php
@@ -1,18 +1,18 @@
 <?php get_header(); ?>
-	
-	<?php while( have_posts() ) the_post(); { ?>
+
+	<?php while( have_posts() ) { the_post(); ?>
 
 		<div class="hero" style="background: url(<?php echo get_the_post_thumbnail_url(); ?>);">
 			<div class="contenido-hero">
 				<div class="texto-hero">
-					<h1><?php the_title(); ?></h1>
+					<h1><?php the_title(); ?></h1> <!-- imprime el titutlo de la página de WP -->
 				</div>
 			</div> <!-- .contenido-hero -->
 		</div> <!-- .hero -->
 
 		<div class="principal contenedor">
 			<main class="texto-centrado contenido-paginas">
-				<?php the_content(); ?>		
+				<?php the_content(); ?> <!-- imprime el contenido de WP -->
 			</main> <!-- contenido-paginas -->
 		</div> <!-- .principal contenedor -->
 
