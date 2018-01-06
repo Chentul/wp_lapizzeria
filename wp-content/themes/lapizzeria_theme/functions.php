d4b115533f931753f22c8541dff6854b11e8912c
vicente noriega <vicente.noriega@thlink.marketing>
2018-01-05 18:21:10 -0600

    SE7 - 82. Creando un Custom Post Type

--- wp-content/themes/lapizzeria_theme/functions.php
+++ wp-content/themes/lapizzeria_theme/functions.php
@@ -58,3 +58,48 @@ function lapizzeria_setup() {
 
 // hook que se corre una vez que se cargaron los archivos del themes
 add_action( 'after_setup_theme', 'lapizzeria_setup' );
+
+/* === CUSTOM POST TYPE
+================================================================*/
+add_action( 'init', 'lapizzeria_especialidades' );
+function lapizzeria_especialidades() {
+	// interfaz
+	$labels = array(
+		'name'               => _x( 'Pizzas', 'lapizzeria' ),
+		'singular_name'      => _x( 'Pizzas', 'post type singular name', 'lapizzeria' ),
+		'menu_name'          => _x( 'Pizzas', 'admin menu', 'lapizzeria' ),
+		'name_admin_bar'     => _x( 'Pizzas', 'add new on admin bar', 'lapizzeria' ),
+		'add_new'            => _x( 'Add New', 'book', 'lapizzeria' ),
+		'add_new_item'       => __( 'Add New Pizza', 'lapizzeria' ),
+		'new_item'           => __( 'New Pizzas', 'lapizzeria' ),
+		'edit_item'          => __( 'Edit Pizzas', 'lapizzeria' ),
+		'view_item'          => __( 'View Pizzas', 'lapizzeria' ),
+		'all_items'          => __( 'All Pizzas', 'lapizzeria' ),
+		'search_items'       => __( 'Search Pizzas', 'lapizzeria' ),
+		'parent_item_colon'  => __( 'Parent Pizzas:', 'lapizzeria' ),
+		'not_found'          => __( 'No Pizzases found.', 'lapizzeria' ),
+		'not_found_in_trash' => __( 'No Pizzases found in Trash.', 'lapizzeria' )
+	);
+	// campos
+	$args = array(
+		'labels'             => $labels,
+    'description'        => __( 'Description.', 'lapizzeria' ),
+		'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'query_var'          => true,
+		// url
+		'rewrite'            => array( 'slug' => 'especialidades' ),
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => 6,
+		// titulo, editor y imagen
+		'supports'           => array( 'title', 'editor', 'thumbnail' ),
+		// habilitamos categorias
+    'taxonomies'          => array( 'category' ),
+	);
+
+	register_post_type( 'especialidades', $args );
+}
