32204ca7a457d24c305c02d9e58b83d95a3abb71
vicentenoriega <vicente.noriega.sc@gmail.com>
2018-01-06 03:16:37 -0600

    SE8 - 90. Agregando entradas al Blog

--- wp-content/themes/lapizzeria_theme/index.php
+++ wp-content/themes/lapizzeria_theme/index.php
@@ -1,13 +1,21 @@
 <?php get_header(); ?>
-	<?php 
-	
-		while( have_posts() ) {
 
-			the_post();
-			the_title( '<h1>', '</h1>' );
-			the_content();
-		}
-		
-	?>
-	<script>console.log('index.php');</script>
+	<?php while( have_posts() ) { the_post(); ?>
+
+		<div class="hero" style="background: url(<?php echo get_the_post_thumbnail_url(); ?>);">
+			<div class="contenido-hero">
+				<div class="texto-hero">
+					<h1><?php the_title(); ?></h1> <!-- imprime el titutlo de la página de WP -->
+				</div>
+			</div> <!-- .contenido-hero -->
+		</div> <!-- .hero -->
+
+		<div class="principal contenedor">
+			<main class="texto-centrado contenido-paginas">
+			</main> <!-- contenido-paginas -->
+		</div> <!-- .principal contenedor -->
+
+	<?php } // fin del while ?>
+
+	<script>console.log('page.php');</script>
 <?php get_footer(); ?>
