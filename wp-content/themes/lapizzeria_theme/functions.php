24c6ebb9efc42509d8feb77a3c69a94efb695c06
vicentenoriega <vicente.noriega.sc@gmail.com>
2018-01-07 11:57:45 -0600

    SE9 - 104. Agregando Fluidbox a nuestro Theme

--- wp-content/themes/lapizzeria_theme/functions.php
+++ wp-content/themes/lapizzeria_theme/functions.php
@@ -8,29 +8,27 @@ function lapizzeria_styles() {
 
 	// función para registrar los estilos
 	wp_register_style( 'normalize', get_template_directory_uri() . '/assets/css/normalize.css', array(), '7.0' );
-
 	wp_register_style( 'google_fonts', 'https://fonts.googleapis.com/css?family=Open+Sans|Raleway:400,700,900', array(), '1.0.0' );
-
 	wp_register_style( 'fontawesome', get_template_directory_uri() . '/assets/css/font-awesome.min.css', array( 'normalize' ), '4.7' );
-
+	wp_register_style( 'fluidboxcss', get_template_directory_uri() . '/assets/css/fluidbox.min.css', array( 'normalize' ), '2.0' );
 	// se agrego en el parametro array( 'normalize' ) como dependencia, es decir, primero carga normalize y seguidamente nuestro style.css
 	wp_register_style( 'style', get_template_directory_uri() . '/style.css', array( 'normalize' ), '1.0' );
 
 	// función para incrustrar los estilos
 	wp_enqueue_style( 'normalize' );
 	wp_enqueue_style( 'fontawesome' );
+	wp_enqueue_style( 'fluidboxcss' );
 	wp_enqueue_style( 'style' );
 
 	/* === SCRIPTS
 	================================================================*/
 	// el ultimo parametro indica que posicione el archivo en el footer
+	wp_register_script( 'fluidboxjs', get_template_directory_uri() . '/assets/js/jquery.fluidbox.min.js', array(), '2.0', true );
 	wp_register_script( 'scripts', get_template_directory_uri() . '/assets/js/scripts.js', array(), '1.0.0', true );
 
-	// incluimos la libreria de jquery a nuestro tema
-	wp_enqueue_script( 'jquery' );
-
-	// incluimos nuestro archivo scripts
-	wp_enqueue_script( 'scripts' );
+	wp_enqueue_script( 'jquery' ); // incluimos la libreria de jquery a nuestro tema
+	wp_enqueue_script( 'fluidboxjs' ); // incluimos plugin de fluidbox
+	wp_enqueue_script( 'scripts' ); // incluimos nuestro archivo scripts
 
 
 }
