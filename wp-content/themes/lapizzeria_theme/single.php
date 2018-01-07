3605cd7f3ffa9194448402ceef225fbc46ddffc2
vicentenoriega <vicente.noriega.sc@gmail.com>
2018-01-07 02:09:00 -0600

    SE8 - 100. Mostrando los comentarios en los post

--- wp-content/themes/lapizzeria_theme/single.php
+++ wp-content/themes/lapizzeria_theme/single.php
@@ -21,6 +21,26 @@
 				// imprime el formulario de los comentarios
 				comment_form();
 			?>
+		</div> <!-- .comentarios -->
+		<div class="contenedor">
+			<ul class="lista-comentarios">
+				<?php 
+					// variable que obtiene todos los comentarios
+					$comentarios = get_comments( array(
+						// solo muestra los comentarios que pertenezcan a este post
+						'post_id' => $post->ID,
+						// solo muestra los comentarios aprovados
+						'status' => 'approve',
+					) );
+					// función que enlista los comentarios recibiendo dos parametros
+					wp_list_comments( array(
+						// solo muestra 10 comentarios por página
+						'per_page' => 10,
+						// muestra el último comentario en top
+						'reverse_top_lever' => false
+					), $comentarios );
+				?>
+			</ul>
 		</div>
 	<?php } // fin del while ?>
 
