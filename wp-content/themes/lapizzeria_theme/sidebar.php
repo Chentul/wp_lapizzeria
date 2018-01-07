58e596aa4d8bcae44e991b576ca7b632f679e069
vicentenoriega <vicente.noriega.sc@gmail.com>
2018-01-06 21:16:53 -0600

    SE8 - 97. Finalizando los estilos para el Sidebar

--- wp-content/themes/lapizzeria_theme/sidebar.php
+++ wp-content/themes/lapizzeria_theme/sidebar.php
@@ -1,4 +1,4 @@
-<aside class="columnas1-3">
+<aside class="columnas1-3 sidebar">
 	<?php 
 		// valida si esta activo el sidebar, en el parametro va el slug que se le indico en el functions.php
 		if( is_active_sidebar( 'blog_sidebar' ) ) {
