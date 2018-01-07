32b233163224496440aa3b441d22e38d1732812e
vicentenoriega <vicente.noriega.sc@gmail.com>
2018-01-07 15:08:25 -0600

    SE11 - 117. Funciones de Sanitizacion de datos

--- wp-content/themes/lapizzeria_theme/inc/reservaciones.php
+++ wp-content/themes/lapizzeria_theme/inc/reservaciones.php
@@ -1,13 +1,19 @@
 <?php
 
 /* === INSERTA LOS DATOS DE RESERVACIONES
-================================================================*/
+================================================================================*/
 function lapizzeria_guardar_datos() {
 
 	if( isset( $_POST[ 'enviar' ] ) && $_POST[ 'oculto' ] == "1" ) {
 
-		
-	}
-}
+		// sanitiza los campos del formulario, por cuestion de seguridad
+		$nombre = sanitize_text_field( $_POST[ 'nombre' ] );
+		$fecha = sanitize_text_field( $_POST[ 'fecha' ] );
+		$correo = sanitize_text_field( $_POST[ 'correo' ] );
+		$telefono = sanitize_text_field( $_POST[ 'telefono' ] );
+		$mensaje = sanitize_text_field( $_POST[ 'mensaje' ] );
+	} // fin del if
+
+} // fin de la funcion lapizzeria_guardar_datos()
 
 add_action( 'init', 'lapizzeria_guardar_datos' );
