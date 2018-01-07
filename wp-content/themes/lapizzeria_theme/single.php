cdfb42cc86a4cce19c608c5a18608affac6a4b47
vicentenoriega <vicente.noriega.sc@gmail.com>
2018-01-06 21:32:44 -0600

    SE8 - 98. Agregando formulario de comentarios

--- wp-content/themes/lapizzeria_theme/single.php
+++ wp-content/themes/lapizzeria_theme/single.php
@@ -1,13 +1,28 @@
 <?php get_header(); ?>
-	<?php 
-	
-		while( have_posts() ) {
 
-			the_post();
-			the_title( '<h1>', '</h1>' );
-			the_content();
-		}
-		
-	?>
-	<script>console.log( 'single.php' );</script>
+	<?php while( have_posts() ) { the_post(); ?>
+
+		<div class="hero" style="background: url(<?php echo get_the_post_thumbnail_url(); ?>);">
+			<div class="contenido-hero">
+				<div class="texto-hero">
+					<h1><?php the_title(); ?></h1> <!-- imprime el titutlo de la página de WP -->
+				</div>
+			</div> <!-- .contenido-hero -->
+		</div> <!-- .hero -->
+
+		<div class="principal contenedor">
+			<main class="texto-centrado contenido-paginas">
+				<?php the_content(); ?> <!-- imprime el contenido de WP -->
+			</main> <!-- contenido-paginas -->
+		</div> <!-- .principal contenedor -->
+
+		<div class="comentarios contenedor">
+			<?php 
+				// imprime el formulario de los comentarios
+				comment_form();
+			?>
+		</div>
+	<?php } // fin del while ?>
+
+	<script>console.log('single.php');</script>
 <?php get_footer(); ?>
